b37490f2fbc552b6b78a16083186047b66b4d68c
Parakkrama Dasanayaka <110714055+Parakkrama24@users.noreply.github.com>
2024-07-08 18:05:15 +0530

    connected momgodb

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+require('dotenv').config();
+const connectDB = require('./db');
 
+// Connect to database
+connectDB();
+
+// Middleware to parse JSON bodies
+app.use(express.json({ extended: false }));
+
+// Define Routes
+app.use('/api/register', require('./config/register'));
+
+// Define a root route
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+// Use the port from environment variables or default to 5000
+const PORT = process.env.PORT || 5000;
+
+// Start the server
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
